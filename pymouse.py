6372ded0eab66493837af421311a409a5d974650
carloscarvallo <erotomania669@gmail.com>
2015-09-29 01:18:39 -0400

    primer commit

---
+++ pymouse.py
@@ -0,0 +1,59 @@
+# -*- coding: iso-8859-1 -*-
+
+"""The goal of PyMouse is to have a cross-platform way to control the mouse.
+PyMouse should work on Windows, Mac and any Unix that has xlib.
+
+See http://code.google.com/p/pymouse/ for more information.
+"""
+
+import sys
+
+class PyMouseMeta(object):
+
+    def press(self, x, y, button = 1):
+        """Press the mouse on a givven x, y and button.
+        Button is defined as 1 = left, 2 = right, 3 = middle."""
+
+        raise NotImplementedError
+
+    def release(self, x, y, button = 1):
+        """Release the mouse on a givven x, y and button.
+        Button is defined as 1 = left, 2 = right, 3 = middle."""
+
+        raise NotImplementedError
+
+    def click(self, x, y, button = 1):
+        """Click the mouse on a givven x, y and button.
+        Button is defined as 1 = left, 2 = right, 3 = middle."""
+
+        self.press(x, y, button)
+        self.release(x, y, button)
+
+    def move(self, x, y):
+        """Move the mouse to a givven x and y"""
+
+        raise NotImplementedError
+
+    def position(self):
+        """Get the current mouse position in pixels.
+        Returns a tuple of 2 integers"""
+
+        raise NotImplementedError
+
+    def screen_size(self):
+        """Get the current screen size in pixels.
+        Returns a tuple of 2 integers"""
+
+        raise NotImplementedError
+
+if sys.platform.startswith('java'):
+    from java_ import PyMouse
+
+elif sys.platform == 'darwin':
+    from mac import PyMouse
+
+elif sys.platform == 'win32':
+    from windows import PyMouse
+
+else:
+    from unix import PyMouse
---
+++ tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+from pymouse import PyMouse
+import random, time
+
+m = PyMouse()
+try:
+	size = m.screen_size()
+	print "size: %s" % (str(size))
+
+	pos = (random.randint(0, size[0]), random.randint(0, size[1]))
+except:
+	pos = (random.randint(0, 250), random.randint(0, 250))
+
+print "Position: %s" % (str(pos))
+
+print 'move'
+m.move(pos[0], pos[1])
+
+time.sleep(2)
+
+print 'click left'
+m.click(pos[0], pos[1], 1)
+
+time.sleep(2)
+
+print 'click right'
+m.click(pos[0], pos[1], 2)
+
+time.sleep(2)
+
+print 'click middle'
+m.click(pos[0], pos[1], 3)
